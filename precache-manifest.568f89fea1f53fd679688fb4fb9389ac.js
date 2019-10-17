12e7f042199380def76cb2fe8908fab1e291d301
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-10-17 10:24:52 +0200

    Updates

---
+++ precache-manifest.568f89fea1f53fd679688fb4fb9389ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c19919bf6a14d23fcdccd3403b97ae",
+    "url": "/StefaniaJ/react-ex/index.html"
+  },
+  {
+    "revision": "560e437d462a057f6250",
+    "url": "/StefaniaJ/react-ex/static/css/main.c8746b23.chunk.css"
+  },
+  {
+    "revision": "c595b820e28f8d776121",
+    "url": "/StefaniaJ/react-ex/static/js/2.babb6a4c.chunk.js"
+  },
+  {
+    "revision": "560e437d462a057f6250",
+    "url": "/StefaniaJ/react-ex/static/js/main.55d05857.chunk.js"
+  },
+  {
+    "revision": "32df23a923599c852599",
+    "url": "/StefaniaJ/react-ex/static/js/runtime-main.81ce44fd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/StefaniaJ/react-ex/static/media/logo.25bf045c.svg"
+  }
+]);
